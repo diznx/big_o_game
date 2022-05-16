d149c90d73c2d7b524a3398bbff166f5351ef961
diznx <85420656+diznx@users.noreply.github.com>
2022-05-16 13:06:24 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,597 @@
+answer_key = {
+'0(1)': '#1',
+'0(log(n))': '#2',
+'0(n)': '#3',
+'0(n log(n))': '#4',
+'0(n^2)': '#5',
+'0(n!)': '#6',
+'0(n(log(n))^2)': '#7',
+'0(nk)' : '#8'
+}
+
+array_access = 1
+array_search = 3
+array_insertion = 3
+array_deletion = 3
+
+stack_access = 3
+stack_search = 3
+stack_insertion = 1
+stack_deletion = 1
+
+queue_access = 3
+queue_search = 3
+queue_insertion = 1
+queue_deletion = 1
+
+singly_linked_list_access = 3
+singly_linked_list_search = 3
+singly_linked_list_insertion = 1
+singly_linked_list_deletion = 1
+
+doubly_linked_list_access = 3
+doubly_linked_list_search = 3
+doubly_linked_list_insertion = 1
+doubly_linked_list_deletion = 1
+
+hash_table_search =3
+hash_table_insertion = 3
+hash_table_deletion = 3
+
+binary_search_tree_access = 3
+binary_search_tree_search = 3
+binary_search_tree_insertion = 3
+binary_search_tree_deletion = 3
+
+
+quick_sort = 6
+merge_sort = 4
+heapsort = 4
+bubble_sort = 6
+insertion_sort = 6
+selection_sort = 6
+tree_sort = 6
+shell_sort = 7
+bucket_sort = 6
+radix_sort = 8
+
+#array questions
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+
+input_array_access = input('time for array access? ')
+
+
+if input_array_access > '1' or input_array_access < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_array_search = input('time for array search? ')
+
+
+if input_array_search > '3' or input_array_search < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+
+input_array_insertion = input('time for array insertion? ')
+
+
+if input_array_insertion > '3' or input_array_insertion < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_array_deletion = input('time for array deletion? ')
+
+
+if input_array_deletion > '3' or input_array_deletion < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+#stack questions
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_stack_access = input('time for stack access? ')
+
+
+if input_stack_access > '3' or input_stack_access < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_stack_search = input('time for stack search? ')
+
+
+if input_stack_search > '3' or input_stack_search < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_stack_insertion = input('time for stack insertion? ')
+
+
+if input_stack_insertion > '1' or input_stack_insertion < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_stack_deletion = input('time for stack deletion? ')
+
+
+if input_stack_deletion > '1' or input_stack_deletion < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+
+
+#queue questions
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_queue_access = input('time for queue access? ')
+
+
+if input_queue_access > '3' or input_queue_access < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_queue_search = input('time for queue search? ')
+
+
+if input_queue_search > '3' or input_queue_search < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_queue_insertion = input('time for queue insertion? ')
+
+
+if input_queue_insertion > '1' or input_queue_insertion < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_queue_deletion = input('time for queue deletion? ')
+
+
+if input_queue_deletion > '1' or input_queue_deletion < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+
+#singly linked lists questions
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_singly_access = input('time for singly linked list access? ')
+
+
+if input_singly_access > '3' or input_singly_access < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_singly_search = input('time for singly linked list search? ')
+
+
+if input_singly_search > '3' or input_singly_search < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_singly_insertion = input('time for singly linked list insertion? ')
+
+
+if input_singly_insertion > '1' or input_singly_insertion < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_singly_deletion = input('time for singly linked list deletion? ')
+
+
+if input_singly_deletion > '1' or input_singly_deletion < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+    
+#doubly linked lists questions
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_doubly_access = input('time for doubly linked list access? ')
+
+
+if input_doubly_access > '3' or input_doubly_access < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_doubly_search = input('time for doubly linked list search? ')
+
+
+if input_doubly_search > '3' or input_doubly_search < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_doubly_insertion = input('time for doubly linked list insertion? ')
+
+
+if input_doubly_insertion > '1' or input_doubly_insertion < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_doubly_deletion = input('time for doubly linked list deletion? ')
+
+
+if input_doubly_deletion > '1' or input_doubly_deletion < '1':
+    print('incorrect')
+else:
+    print('correct')
+
+#binary search tree method
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+binary_search_tree_access = input('time for binary search tree access? ')
+
+
+if binary_search_tree_access > '3' or binary_search_tree_access < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+binary_search_tree_search = input('time for binary search tree search? ')
+
+
+if binary_search_tree_search > '3' or binary_search_tree_search < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+binary_search_tree_insertion = input('time for binary search tree insertion? ')
+
+
+if binary_search_tree_insertion > '3' or binary_search_tree_insertion < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+binary_search_tree_deletion = input('time for binary search tree deletion? ')
+
+
+if binary_search_tree_deletion > '3' or binary_search_tree_deletion < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+#hash table method
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+hash_table_search = input('time for hash table search? ')
+
+
+if hash_table_search > '3' or hash_table_search < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+hash_table_insertion = input('time for hash table insertion? ')
+
+
+if hash_table_insertion > '3' or hash_table_insertion < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+hash_table_deletion = input('time for hash table deletion? ')
+
+
+if hash_table_deletion > '3' or hash_table_deletion < '3':
+    print('incorrect')
+else:
+    print('correct')
+
+
+
+
+
+#sorting method questions
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_quick_sort = input('time for quick sort? ')
+
+
+if input_quick_sort > '5' or input_quick_sort < '5':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_merge_sort = input('time for merge sort? ')
+
+
+if input_merge_sort > '4' or input_merge_sort < '4':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_heap_sort = input('time for heap sort? ')
+
+
+if input_heap_sort > '4' or input_heap_sort < '4':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_bubble_sort = input('time for bubble sort? ')
+
+
+if input_bubble_sort > '5' or input_bubble_sort < '5':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_insertion_sort = input('time for insertion sort? ')
+
+
+if input_insertion_sort > '5' or input_insertion_sort < '5':
+    print('incorrect')
+else:
+    print('correct')
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_selection_sort = input('time for selection sort? ')
+
+
+if input_selection_sort > '5' or input_selection_sort < '5':
+    print('incorrect')
+else:
+    print('correct')
+
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_tree_sort = input('time for tree sort? ')
+
+
+if input_tree_sort > '5' or input_tree_sort < '5':
+    print('incorrect')
+else:
+    print('correct')
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_shell_sort = input('time for shell sort? ')
+
+
+if input_shell_sort > '7' or input_shell_sort < '7':
+    print('incorrect')
+else:
+    print('correct')
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_bucket_sort = input('time for bucket sort? ')
+
+
+if input_bucket_sort > '5' or input_bucket_sort < '5':
+    print('incorrect')
+else:
+    print('correct')
+
+for key in answer_key:
+    print(key, answer_key[key])
+    print()
+    print()
+    
+input_radix_sort = input('time for radix sort? ')
+
+if input_radix_sort > '8' or input_radix_sort < '8':
+    print('incorrect')
+else:
+    print('correct')
+
